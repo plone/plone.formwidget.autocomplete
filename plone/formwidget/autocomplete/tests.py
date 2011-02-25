adec458a7aaf6065cd4ef359d523806417c7221b
Hanno Schlichting <hanno@hannosch.eu>
2011-02-25 13:08:07 +0000

    Remove direct `zope.app` dependencies.
    svn path=/plone.formwidget.autocomplete/trunk/; revision=47737

--- plone/formwidget/autocomplete/tests.py
+++ plone/formwidget/autocomplete/tests.py
@@ -1,16 +1,30 @@
-import os
+import doctest
 import unittest
 
-from zope.testing import doctest
-from zope.app.testing.functional import ZCMLLayer
+from Testing.ZopeTestCase.layer import ZopeLite
 
-testing_zcml_path = os.path.join(os.path.dirname(__file__), 'testing.zcml')
-testing_zcml_layer = ZCMLLayer(testing_zcml_path, 'plone.formwidget.autocomplete', 'testing_zcml_layer')
+# BBB for Zope 2.12
+try:
+    from Zope2.App import zcml
+except ImportError:
+    from Products.Five import zcml
+
+
+class ZCMLLayer(ZopeLite):
+
+    @classmethod
+    def testSetUp(cls):
+        import plone.formwidget.autocomplete
+        zcml.load_config('testing.zcml', plone.formwidget.autocomplete)
+
+    @classmethod
+    def testTearDown(cls):
+        pass
+
 
 def test_suite():
     readme_txt = doctest.DocFileSuite('README.txt')
-    readme_txt.layer = testing_zcml_layer
-    
+    readme_txt.layer = ZCMLLayer
     return unittest.TestSuite([
         readme_txt,
         ])
