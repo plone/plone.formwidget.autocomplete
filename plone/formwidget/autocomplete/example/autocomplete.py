7fef19a2010587f92520195a11b691dae1369b91
JeanMichel FRANCOIS <toutpt@gmail.com>
2013-03-08 13:51:24 +0100

    move the demo into example

--- plone/formwidget/autocomplete/example/autocomplete.py
+++ plone/formwidget/autocomplete/example/autocomplete.py
@@ -1,77 +1,97 @@
-from zope import component
-from zope import interface
-from zope import schema
-from zope.schema.vocabulary import SimpleVocabulary, SimpleTerm
-from zope.schema.interfaces import IContextSourceBinder
-
+from plone.z3cform import layout
+from Products.CMFCore.utils import getToolByName
+from z3c.form import form, button, field
 from z3c.formwidget.query.interfaces import IQuerySource
-from z3c.form.interfaces import IFieldsForm
-from z3c.form import form, field, button
+from zope.component import adapts
+from zope.interface import Interface, implements
+from zope import schema
+from zope.schema.interfaces import IContextSourceBinder
+from zope.schema.vocabulary import SimpleVocabulary, SimpleTerm
 
-from Products.Five import BrowserView
-
-from plone.autoform.form import AutoExtensibleForm
 from plone.formwidget.autocomplete import AutocompleteFieldWidget
 from plone.formwidget.autocomplete import AutocompleteMultiFieldWidget
-from plone.z3cform.layout import wrap_form
 
 
-class ItalianCities(object):
-    interface.implements(IQuerySource)
-
-    vocabulary = SimpleVocabulary((
-        SimpleTerm(u'Bologna', 'bologna', u'Bologna'),
-        SimpleTerm(u'Palermo', 'palermo', u'Palermo'),
-        SimpleTerm(u'Sorrento', 'sorrento', u'Sorrento'),
-        SimpleTerm(u'Torino', 'torino', u'Torino')))
+class KeywordSource(object):
+    implements(IQuerySource)
 
     def __init__(self, context):
         self.context = context
+        catalog = getToolByName(context, 'portal_catalog')
+        self.keywords = catalog.uniqueValuesFor('Subject')
+        terms = []
+        for x in self.keywords:
+            terms.append(SimpleTerm(x, x, unicode(x)))
+        self.vocab = SimpleVocabulary(terms)
 
-    __contains__ = vocabulary.__contains__
-    __iter__ = vocabulary.__iter__
-    getTerm = vocabulary.getTerm
-    getTermByToken = vocabulary.getTermByToken
+    def __contains__(self, term):
+        return self.vocab.__contains__(term)
+
+    def __iter__(self):
+        return self.vocab.__iter__()
+
+    def __len__(self):
+        return self.vocab.__len__()
+
+    def getTerm(self, value):
+        return self.vocab.getTerm(value)
+
+    def getTermByToken(self, value):
+        return self.vocab.getTermByToken(value)
 
     def search(self, query_string):
-        return [v for v in self if query_string.lower() in v.value.lower()]
+        q = query_string.lower()
+        return [self.getTerm(kw) for kw in self.keywords if q in kw.lower()]
 
 
-class ItalianCitiesSourceBinder(object):
-    interface.implements(IContextSourceBinder)
+class KeywordSourceBinder(object):
+    implements(IContextSourceBinder)
 
     def __call__(self, context):
-        return ItalianCities(context)
+        return KeywordSource(context)
 
 
-class ICities(interface.Interface):
-    favourite_city = schema.Choice(title=u"Favourite city",
-                                   source=ItalianCitiesSourceBinder())
-    visited_cities = schema.List(
-        title=u"Visited cities",
-        value_type=schema.Choice(title=u"Selection",
-                                 source=ItalianCitiesSourceBinder()))
+class ITestForm(Interface):
+
+    single_keyword = schema.Choice(title=u"Single",
+        source=KeywordSourceBinder(), required=False)
+
+    keywords = schema.List(title=u"Multiple", value_type=schema.Choice(
+        title=u"Multiple", source=KeywordSourceBinder()), required=False)
 
 
-class CitiesForm(form.Form):
-    interface.implements(ICities)
-    fields = field.Fields(ICities)
-    fields['favourite_city'].widgetFactory = AutocompleteFieldWidget
-    fields['visited_cities'].widgetFactory = AutocompleteMultiFieldWidget
-
-    @button.buttonAndHandler(u'Apply')
-    def handleApply(self, action):
-        data, errors = self.extractData()
-        print "Submitted data:", data
-
-form_view = wrap_form(CitiesForm)
-
-
-class CitiesFormAdapter(object):
-    component.adapts(interface.Interface)
-    interface.implements(ICities)
+class TestAdapter(object):
+    implements(ITestForm)
+    adapts(Interface)
 
     def __init__(self, context):
         self.context = context
-        self.visited_cities = None
-        self.favourite_city = None
+
+    def _get_single_keyword(self):
+        return self.context.Subject() and self.context.Subject()[0] or None
+
+    def _set_single_keyword(self, value):
+        print "setting", value
+
+    single_keyword = property(_get_single_keyword, _set_single_keyword)
+
+    def _get_keywords(self):
+        return self.context.Subject()
+
+    def _set_keywords(self, value):
+        print "setting", value
+
+    keywords = property(_get_keywords, _set_keywords)
+
+
+class TestForm(form.Form):
+    fields = field.Fields(ITestForm)
+    fields['single_keyword'].widgetFactory = AutocompleteFieldWidget
+    fields['keywords'].widgetFactory = AutocompleteMultiFieldWidget
+
+    @button.buttonAndHandler(u'Ok')
+    def handle_ok(self, action):
+        data, errors = self.extractData()
+        print data, errors
+
+TestView = layout.wrap_form(TestForm)
